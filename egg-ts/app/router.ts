34e368e111de63c20d4b67a7325b7b374d674a93
mircool <ynyjyhc@live.cn>
2023-01-13 09:19:01 +0800

    处理通用接口,未完成

--- egg-ts/app/router.ts
+++ egg-ts/app/router.ts
@@ -5,11 +5,15 @@ export default (app: Application) => {
 
     router.get('/', controller.home.index);
     router.post('/api/admin/login', controller.api.admin.login);    //登录
-    // router.get('/api/rest/items', controller.api.item.list);    //获取所有信息
-    // router.post('/api/rest/items', controller.api.item.create); //创建信息
-    // router.delete('/api/rest/items/:id', controller.api.item.delete);   //删除信息
-    // router.put('/api/rest/items/:id', controller.api.item.update);  //更新信息
-    // router.get('/api/rest/items/:id', controller.api.item.show);    //获取信息
-    router.resources('items', '/api/rest/items', controller.api.item);    //资源路由
     router.post('upload', '/api/upload', controller.api.upload.create);    //文件上传
+    router.resources('model', '/api/rest/:model', async (ctx, next) => {
+        console.log('ctx:', ctx.model);
+        // const modelName = require('inflection').classify(ctx.params.model)    //转换成类名
+        // console.log('modelName:', modelName);
+        // ctx.model = ctx.model[modelName];
+        // console.log('ctx.model:', ctx.model);
+        await next();
+
+    }, controller.api.item);    //资源路由
+
 };
