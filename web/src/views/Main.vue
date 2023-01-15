568951af0b969b181ba4bf248ad7a8e79720a7e0
mircool <ynyjyhc@live.cn>
2023-01-15 22:51:32 +0800

    开发导航

---
+++ web/src/views/Main.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+  <div class="topbar bg-black py-2 px-3 d-flex ai-center">
+    <img src="../assets/logo.png" alt="pvp" height="32" width="32">
+    <div class="px-2 flex-1">
+      <div class="text-white">王者荣耀</div>
+      <div class="text-grey-1 fs-xxs">团队成就更多</div>
+    </div>
+    <button type="button" class="btn bg-primary">立即下载</button>
+  </div>
+  <div class="bg-primary pt-3 pb-2">
+    <div class="nav d-flex jc-around text-white pb-1">
+      <div class="nav-item ">
+        <router-link to="/" class="nav-link text-white active">首页</router-link>
+      </div>
+      <div class="nav-item">
+        <router-link to="/about" class="nav-link text-white">攻略中心</router-link>
+      </div>
+      <div class="nav-item">
+        <router-link to="/about" class="nav-link text-white">赛事中心</router-link>
+      </div>
+
+    </div>
+
+  </div>
+  <RouterView/>
+</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
