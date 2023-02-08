2fdc6550585bd62025ff56319148165fc3a596ad
mircool <ynyjyz@163.com>
2023-02-08 16:04:03 +0800

    封装卡片组件

--- web/src/components/Card.vue
+++ web/src/components/Card.vue
@@ -1,30 +1,12 @@
 <template>
   <div class="card bg-white p-3 mt-3">
-    <div class="card-head d-flex ai-center">
+    <div class="card-head d-flex ai-center pb-3">
       <i :class="`iconfont icon-${icon}`"></i>
       <div class="fs-xl flex-1 px-2">{{ title }}</div>
       <i class="iconfont icon-gengduo"></i>
     </div>
     <div class="card-body pt-3">
-      <div class="nav jc-between">
-        <div class="nav-item active">热门</div>
-        <div class="nav-item">新闻</div>
-        <div class="nav-item">公告</div>
-        <div class="nav-item">活动</div>
-        <div class="nav-item">赛事</div>
-      </div>
-      <div class="mt-2">
-        <swiper>
-          <swiper-slide v-for="m in 5" :key="m">
-            <div class="py-2" v-for="n in 5" :key="n">
-              <span>[新闻]</span>
-              <span>|</span>
-              <span>2月2日全服不停机更新公告</span>
-              <span>02/01</span>
-            </div>
-          </swiper-slide>
-        </swiper>
-      </div>
+      <slot></slot>
     </div>
   </div>
 </template>
@@ -47,6 +29,14 @@ const props = defineProps({
 })
 </script>
 
-<style scoped>
+<style lang="scss">
+@import '../assets/scss/variables.scss';
+.card{
+  border-radius: 5px;
+  .card-head {
+    border-bottom: 1px solid $border-color;
+  }
+  border-bottom: 1px solid $border-color;
+}
 
 </style>
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -22,7 +22,31 @@
       </div>
     </div>
     <!--    end of nav-icons-->
-    <card title="新闻资讯" icon="menu"></card>
+    <card title="新闻资讯" icon="menu">
+      <div class="nav jc-between">
+        <div class="nav-item active">热门</div>
+        <div class="nav-item">新闻</div>
+        <div class="nav-item">公告</div>
+        <div class="nav-item">活动</div>
+        <div class="nav-item">赛事</div>
+      </div>
+      <div class="mt-2">
+        <swiper>
+          <swiper-slide v-for="m in 5" :key="m">
+            <div class="py-2" v-for="n in 5" :key="n">
+              <span>[新闻]</span>
+              <span>|</span>
+              <span>2月2日全服不停机更新公告</span>
+              <span>02/01</span>
+            </div>
+          </swiper-slide>
+        </swiper>
+      </div>
+    </card>
+    <card title="英雄列表">
+    </card>
+    <card title="英雄列表">
+    </card>
   </div>
 </template>
 
