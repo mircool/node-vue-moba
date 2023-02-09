cfa5eb00909fc02b632e207deff07daef32e5950
mircool <ynyjyz@163.com>
2023-02-09 10:49:01 +0800

    封装列表卡片

---
+++ web/src/components/ListCard.vue
@@ -0,0 +1,49 @@
+<template>
+  <card :title="title" :icon="icon">
+    <div class="nav jc-between">
+      <div class="nav-item" :class="{active:false}">
+        <div class="nav-link">热门</div>
+      </div>
+    </div>
+    <div class="mt-2">
+      <swiper>
+        <swiper-slide v-for="m in 5" :key="m">
+          <div class="py-2" v-for="n in 5" :key="n">
+            <span>[新闻]</span>
+            <span>|</span>
+            <span>2月2日全服不停机更新公告</span>
+            <span>02/01</span>
+          </div>
+        </swiper-slide>
+      </swiper>
+    </div>
+  </card>
+</template>
+
+<script setup>
+import Card from "./Card.vue";
+import SwiperClass, {Pagination, Autoplay} from 'swiper'
+import {Swiper, SwiperSlide} from 'vue-awesome-swiper'
+import 'swiper/css'
+import 'swiper/css/pagination'
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  icon: {
+    type: String,
+    required: true
+  },
+  categories: {
+    type: Array,
+    required: true
+  }
+})
+
+</script>
+
+<style scoped>
+
+</style>
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -22,30 +22,19 @@
       </div>
     </div>
     <!--    end of nav-icons-->
-    <card title="新闻资讯" icon="menu">
-      <div class="nav jc-between">
-        <div class="nav-item active">热门</div>
-        <div class="nav-item">新闻</div>
-        <div class="nav-item">公告</div>
-        <div class="nav-item">活动</div>
-        <div class="nav-item">赛事</div>
+    <list-card title="" icon="" :categories="newsCats">
+      <div class="py-2" v-for="n in 5" :key="n">
+        <span>[新闻]</span>
+        <span>|</span>
+        <span>2月2日全服不停机更新公告</span>
+        <span>02/01</span>
       </div>
-      <div class="mt-2">
-        <swiper>
-          <swiper-slide v-for="m in 5" :key="m">
-            <div class="py-2" v-for="n in 5" :key="n">
-              <span>[新闻]</span>
-              <span>|</span>
-              <span>2月2日全服不停机更新公告</span>
-              <span>02/01</span>
-            </div>
-          </swiper-slide>
-        </swiper>
-      </div>
+    </list-card>
+    <card title="英雄列表" icon="menu">
     </card>
-    <card title="英雄列表">
+    <card title="精彩视频" icon="menu">
     </card>
-    <card title="英雄列表">
+    <card title="图文攻略" icon="menu">
     </card>
   </div>
 </template>
@@ -58,8 +47,11 @@ import 'swiper/css/pagination'
 
 import {Bill, More} from '@icon-park/vue-next';
 import Card from "../components/Card.vue";
+import ListCard from "../components/ListCard.vue";
 
 const modules = [Pagination, Autoplay]
+
+const newsCats =
 
 </script>
 
